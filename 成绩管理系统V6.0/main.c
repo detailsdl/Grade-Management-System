32a701d628a255881c2234a994925cccddb49316
李展 <lizhan666@li.local>
2015-11-17 18:29:44 +0800

    Initial Commit

---
+++ 成绩管理系统V6.0/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  成绩管理系统V6.0
+//
+//  Created by 李展 on 15/11/17.
+//  Copyright © 2015年 李展. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
